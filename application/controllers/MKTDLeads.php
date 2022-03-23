1d8e50ffdcafb5b97002ee09329fe4dd75e7d605
Christian Sanchez <programador.analista18@ciudadmaderas.com>
2022-03-23 13:57:50 -0600

    servidio dragon 1.1

---
+++ application/controllers/MKTDLeads.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MKTDLeads extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model(array('Postventa_model', 'Documentacion_model', 'General_model'));
+        $this->load->library(array('session', 'form_validation', 'get_menu'));
+        date_default_timezone_set('America/Mexico_City');
+    }
+
+    public function index_post()
+	{
+		$input = $this->input->post();
+        print_r($input);
+		$this->db->insert('prospectos',$input);
+		$this->response(['Lead Insertado correctamente.'], REST_Controller::HTTP_OK);
+	}
+
+}
+
+
