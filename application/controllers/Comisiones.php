d36d3904c3d7e3f4e3d9f8ec7635be3d7d60b013
ozuna16 <80979535+ozuna16@users.noreply.github.com>
2022-04-12 09:03:51 -0500

    Borrar 2 funciones

--- application/controllers/Comisiones.php
+++ application/controllers/Comisiones.php
@@ -222,10 +222,7 @@ public function getPuestosDescuentos(){
       $this->load->view('ventas/flujo_comisiones', $datos);
     }
 
-    public function getDatosFlujoComisiones() {
-      $data = $this->Comisiones_model->getDatosFlujoComisiones()->result_array();
-      echo json_encode(array('data' => $data));
-    }
+    
     
     function aprobar_comision(){
       $id_pago= $_POST['id_pago'];
@@ -6441,15 +6438,9 @@ for ($d=0; $d <count($dos) ; $d++) {
         exit;
     }
 
-    public function flujo_comisiones() {
-      $datos = array();
-      $datos["datos2"] = $this->Asesor_model->getMenu($this->session->userdata('id_rol'))->result();
-      $datos["datos3"] = $this->Asesor_model->getMenuHijos($this->session->userdata('id_rol'))->result();
-      $val = "https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
-      $salida = str_replace('' . base_url() . '', '', $val);
-      $datos["datos4"] = $this->Asesor_model->getActiveBtn($salida, $this->session->userdata('id_rol'))->result();
 
     /**--------------------------------PRESTAMOS ATOMATICOS--------------------- */
+    
     public function panel_prestamos()
     {
 
