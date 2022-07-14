f471cf73c57314d70738908b7cc2ed946ee67048
programadorareli21 <100244501+programadorareli21@users.noreply.github.com>
2022-07-14 17:24:56 -0500

    cambios dashboard v2

--- application/views/dashboard/agenda/calendar.php
+++ application/views/dashboard/agenda/calendar.php
@@ -7,6 +7,7 @@
                 <!-- Subdirector -->
                 <?php if( $this->session->userdata('id_rol') == 2 ) { ?>
                     <div class="col-12 col-sm-4 col-md-4 col-lg-4 overflow-hidden pl-0">
+                        <label class="label-gral">Gerente</label>
                         <select class="selectpicker select-gral m-0" id="gerente" name="gerente" data-style="btn" data-show-subtext="true" data-live-search="true" title="Selecciona un gerente" data-size="7" data-container="body"></select>
                     </div>
                 <?php } ?>
@@ -17,6 +18,7 @@
                     <?php } else  { ?> 
                     <div class="col-12 col-sm-6 col-md-6 col-lg-6 overflow-hidden pl-0" >
                     <?php } ?>
+                        <label class="label-gral" id="labelCoord" style="display:none">Coordinador</label>
                         <select class="selectpicker select-gral m-0" id="coordinador" name="coordinador" data-style="btn" data-show-subtext="true" data-live-search="true" title="Selecciona un coordinador" data-size="7" data-container="body"></select> 
                     </div>
                     <?php if( $this->session->userdata('id_rol') == 2 ) { ?>
@@ -24,6 +26,7 @@
                     <?php } else  { ?> 
                     <div class="col-12 col-sm-6 col-md-6 col-lg-6 overflow-hidden pr-0">
                     <?php } ?>
+                        <label class="label-gral" id="labelAses" style="display:none">Asesor</label>
                         <select class="selectpicker select-gral m-0" id="asesor" name="asesor" data-style="btn" data-show-subtext="true" data-live-search="true" title="Selecciona un asesor" data-size="7" data-container="body"></select>
                     </div>
                 <?php } ?>
@@ -38,26 +41,6 @@
                 <div class="row">
                     <div class="col-12 col-sm-12 col-md-12 col-lg-12 p-0">
                         <div id='calendar'></div>
-                    </div>
-                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 p-0">
-                        <div class="boxCodigos d-flex">
-                            <div class="boxStatus d-flex">
-                                <i class="fas fa-circle codeFinish"></i>
-                                <p class="m-0">Cita finalizada</p>
-                            </div>
-                            <div class="boxStatus d-flex">
-                                <i class="fas fa-circle codeOpen"></i>
-                                <p class="m-0">Cita abierta</p>
-                            </div>
-                            <div class="boxStatus d-flex">
-                                <i class="fas fa-square codeAdviser"></i>
-                                <p class="m-0">Evento de asesor de ventas</p>
-                            </div>
-                            <div class="boxStatus d-flex">
-                                <i class="fas fa-square codeManager"></i>
-                                <p class="m-0">Evento de coordinador</p>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
