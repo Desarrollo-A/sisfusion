f35f162b25aecee6f8fef4cd4ffab72916da14fc
mgarmarcm <mariadejesus.garduno@ciudadmaderas.com>
2022-07-18 19:11:30 -0500

    Cambios validación de evidencia

--- application/controllers/Caja_outside.php
+++ application/controllers/Caja_outside.php
@@ -2621,36 +2621,6 @@ class Caja_outside extends CI_Controller
         }
     }
 
-    public function generateToken()
-    {
-        $datos = $this->get_menu->get_menu_data($this->session->userdata('id_rol'));
-        $this->load->view('template/header');
-        $this->load->view("token/generateToken", $datos);
-    }
-
-    public function getTokensInformation()
-    {
-        $data['data'] = $this->caja_model_outside->getTokensInformation()->result_array();
-        echo json_encode($data, JSON_NUMERIC_CHECK);
-    }
-
-    public function reviewTokenEvidence()
-    {
-        $datos = $this->get_menu->get_menu_data($this->session->userdata('id_rol'));
-        $this->load->view('template/header');
-        $this->load->view("token/reviewTokenEvidence", $datos);
-    }
-
-    public function validarToken()
-    {
-        if(isset($_POST) && !empty($_POST)){
-            $data = array ("estatus" => $this->input->post("action"));
-            $response = $this->General_model->updateRecord('tokens',  $data, 'id_token', $this->input->post("id"));
-            echo json_encode($response);
-        }
-
-    }
-
     public function getEmpresasLargoList()
     {
         echo json_encode($this->caja_model_outside->getEmpresasLargoList());
