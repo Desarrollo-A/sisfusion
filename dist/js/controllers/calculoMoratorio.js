3fdf6eca0e90d97ad3976f940d7ae14dc04d382e
ecc718 <programador.analista8@ciudadmaderas.com>
2022-07-22 18:05:11 -0500

    Cambios de moratorios yola
    cambios en la primera seccion de 0 a 36 MSI

--- dist/js/controllers/calculoMoratorio.js
+++ dist/js/controllers/calculoMoratorio.js
@@ -1008,25 +1008,21 @@ function calculoMoratorioII(IM, importeSaldoI, posPay, PositionPago, diasRetardo
 											//$scope.infoMoratorio.si
 											//saldo =  $scope.infoMoratorio.si =(($scope.infoMoratorio.si-$scope.alphaNumeric[i]['capital']))
 
-											if($('#msiField').val()>0)
-											{
-												if($scope.total2 > 0)
-												{
-													saldo=$scope.total2;
-												}
-												else
-												{
-													saldo=$scope.alphaNumeric[x]['saldo'];
-												}
-											}
-											else
+											if($('#msiField').val()<= posicionPago )
 											{
 												saldo=$scope.alphaNumeric[x]['saldo'];
 											}
-											// alert(i + " this is saldo " + $scope.alphaNumeric[x]['saldo']);
+											else
+											{
+												saldo=$scope.alphaNumeric[posPay]['saldo'];
+
+											}
+
+											// saldo=$scope.alphaNumeric[posPay]['saldo']
 										}
 										else
 										{
+											//cambia todos los que están antes de ese pago
 											for(var x=0; x < posicionPago ; x++)
 											{
 												saldo= $scope.infoMoratorio.si = $scope.alphaNumeric[i]['saldo'];
@@ -1040,7 +1036,7 @@ function calculoMoratorioII(IM, importeSaldoI, posPay, PositionPago, diasRetardo
 									}
 									else
 									{
-										saldo =  $scope.infoMoratorio.si =$scope.infoMoratorio.si-$scope.alphaNumeric[posPay]['total'];
+										saldo =  $scope.infoMoratorio.si = $scope.infoMoratorio.si-$scope.alphaNumeric[posPay]['total'];
 									}
 								}
 								else
@@ -1250,7 +1246,62 @@ function calculoMoratorioII(IM, importeSaldoI, posPay, PositionPago, diasRetardo
 								// return inputCapital+numberPay;
 								if($scope.alphaNumeric[(full['pago']-1)]['disp']==1 && $scope.alphaNumeric[(full['pago']-1)]['importe']!="" && full['importe']!="")//
 								{
-										var inputCapital = '<input name="importe' + full["pago"] + '" type="number" id="idImporte' + full["pago"] + '"   placeholder="Importe" class="form-control" value="' + full['importe'] + '">';//onchange="pagoCapChange('+full["pago"]+')"
+										// var inputCapital = '<input name="importe' + full["pago"] + '"  id="idImporte' + full["pago"] + '"   placeholder="Importe" class="form-control" value="' +full['importe'] + '" type="tel" pattern="^\\$\\d{1,3}(,\\d{3})*(\\.\\d+)?$"  data-type="currency" >';//onchange="pagoCapChange('+full["pago"]+')"
+									var input_val = full['importe'];
+
+									// don't validate empty input
+									if (input_val === "") { return; }
+
+									// original length
+									var original_len = input_val.length;
+
+
+
+									// check for decimal
+									if (input_val.indexOf(".") >= 0) {
+
+										// get position of first decimal
+										// this prevents multiple decimals from
+										// being entered
+										var decimal_pos = input_val.indexOf(".");
+
+										// split number by decimal point
+										var left_side = input_val.substring(0, decimal_pos);
+										var right_side = input_val.substring(decimal_pos);
+
+										// add commas to left side of number
+										left_side = formatNumber(left_side);
+
+										// validate right side
+										right_side = formatNumber(right_side);
+
+										// On blur make sure 2 numbers after decimal
+										if (blur === "blur") {
+											right_side += "00";
+										}
+
+										// Limit decimal to only 2 digits
+										right_side = right_side.substring(0, 2);
+
+										// join number by .
+										input_val = "$" + left_side + "." + right_side;
+
+									} else {
+										// no decimal entered
+										// add commas to number
+										// remove all non-digits
+										input_val = formatNumber(input_val);
+										input_val = "$" + input_val;
+
+										// final formatting
+										if (blur === "blur") {
+											input_val += ".00";
+										}
+									}
+									// console.log(input_val);
+										var inputCapital = input_val;
+										// let importeMoney = formatNumber(full['importe']);
+										// console.log(importeMoney);
 										var numberPay = '<input name="numberPay' + full["pago"] + '" type="hidden" id="payNum' + full["pago"] + '" value="' + full["pago"] + '">';
 										return inputCapital + numberPay;
 								}
