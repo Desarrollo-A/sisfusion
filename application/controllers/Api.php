e5aa0f43c49ece889f02fd035a220cd851495590
Aureaac <programador.analista12@ciudadmaderas.com>
2022-08-22 09:15:55 -0500

    se quitan funciones no usadas

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -330,8 +330,7 @@ class Api extends CI_Controller
     }
 
    
-    public function external_dashboard()
-    {
+    public function external_dashboard(){
         $response = $this->validateToken_dashboard($_GET['tkn']);
         $res = json_decode($response);
         if($res->status == 200){
@@ -346,25 +345,5 @@ class Api extends CI_Controller
         }else{
             die("Inicio de sesion caducado.");
         }
-    }
-
-    public function external_dashboardAu()
-    {
-        preg_match('/Bearer\s(\S+)/', apache_request_headers()['Authorization'], $matches);
-        $tkn = $matches[1];
-        $validate = json_decode($this->validateToken_dashboard($tkn));
-
-        if($validate->status == 200){
-            $this->session->set_userdata(array(
-                'id_rol'  => $validate->data->data->rol,
-                'id_usuario' => $validate->data->data->id_usuario
-            ));
-            $datos['sub_menu'] = $this->get_menu->get_submenu_data($this->session->userdata('id_rol'), $this->session->userdata('id_usuario'));
-            $datos['external'] = true;
-            $this->load->view('template/header');
-            $this->load->view("dashboard/base/base", $datos);
-        }else{
-            die("Acceso denegado");
-        } 
     }
 }
