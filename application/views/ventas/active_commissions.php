aa78822605721984abf40709d7ceea81d57f8be8
programador21D <100244501+programadorareli21@users.noreply.github.com>
2022-10-04 08:35:37 -0500

    test

--- application/views/ventas/active_commissions.php
+++ application/views/ventas/active_commissions.php
@@ -2,6 +2,7 @@
 <link href="<?= base_url() ?>dist/css/datatableNFilters.css" rel="stylesheet"/>
 <body class="">
     <div class="wrapper ">
+        
         <?php
         if ($this->session->userdata('id_rol') == "13" || $this->session->userdata('id_rol') == "17" || $this->session->userdata('id_rol') == "32" || $this->session->userdata('id_rol') == "8"){
             $datos = array();
