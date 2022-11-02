0db8c92e982d633ef12cd48953f6e29e891001e6
Aureaac <programador.analista12@ciudadmaderas.com>
2022-11-02 10:16:26 -0600

    corrección validación tipo venta, vista contraloria

--- application/views/contraloria/vista_envio_RL_contraloria.php
+++ application/views/contraloria/vista_envio_RL_contraloria.php
@@ -28,8 +28,12 @@
                                 <h3 class="card-title center-align">Envío contrato a RL </h3>
                                 <p class="card-title pl-1">(estatus 10)</p>
                             </div>
-                            
-                            <div  class="toolbar">
+
+                             <?php 
+
+                                    if ($this->session->userdata('id_rol') != "63"){?>
+
+                                    <div  class="toolbar">
                                 <div class="row">
                                     <div class="col col-xs-12 col-sm-12 col-md-3 col-lg-3 pb-5">
                                         <button class="btn-gral-data sendCont">Enviar contratos <i class="fas fa-paper-plane pl-1"></i></button>
@@ -37,7 +41,10 @@
                                 </div>
                             </div>
 
+                                   <?php }?>
 
+                                   
+                            
                             <div class="material-datatables">
                                 <div class="table-responsive">
                                     <table id="tabla_envio_RL" name="tabla_envio_RL" class="table-striped table-hover">
@@ -67,8 +74,8 @@
                                             </div>
                                         </div>
                                         <div class="modal-footer">
-                                            <button type="button" class="btn btn-danger btn-simple" data-dismiss="modal">Cancelar</button>
-                                            <button type="button" id="btn_show" class="btn btn-primary">Enviar Contratos</button>
+                                            <button type="button" id="btn_show" class="btn btn-success"><span class="material-icons">send</span> </i> Enviar Contratos</button>
+                                            <button type="button" class="btn btn-danger" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancelar</button>
                                             <br>
                                         </div>
                                     </div>
@@ -82,6 +89,70 @@
     </div>
 
 
+    <div class="content hide">
+        <div class="container-fluid">
+ 
+            <div class="row">
+
+                <div class="col xol-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="card-header card-header-icon" data-background-color="goldMaderas">
+                            <i class="material-icons">reorder</i>
+                        </div>
+                        <div class="card-content">
+                            <h4 class="card-title center-align">Envío contrato a RL (estatus 10)</h4>
+                            <div class="material-datatables">
+                                <div class="form-group">
+									<div class="modal fade" id="enviarContratos" data-backdrop="static" data-keyboard="false">
+										<div class="modal-dialog modal-md">
+											<div class="modal-content" >
+												<div class="modal-body">
+													<div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
+														<label>Ingresa los códigos de los contratos a enviar: </label>
+														<textarea name="txt" id="contratos" onkeydown="saltoLinea(value);
+														return true;" class="form-control" style="text-transform:uppercase;
+														min-height: 400px;width: 100%"></textarea><br><br>
+													</div>
+												</div>
+												<div class="modal-footer">												
+													<button type="button" id="btn_show" class="btn btn-success"><span class="material-icons">send</span> </i> Enviar Contratos</button>
+					                            	<button type="button" class="btn btn-danger" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancelar</button>
+													<br>
+												</div>
+											</div>
+										</div>
+									</div>
+
+
+                                <div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
+									<button class="btn btn-primary sendCont">Enviar contratos <span class="material-icons">chevron_right</span></button>
+                                        <div class="table-responsive">
+                                        <table class="table table-responsive table-bordered table-striped table-hover"
+                                               id="tabla_envio_RL" name="tabla_envio_RL" style="text-align:center;">
+                                        <thead>
+                                            <tr>
+                                                <th></th>
+												<th style="font-size: .9em;">PROYECTO</th>
+												<th style="font-size: .9em;">CONDOMINIO</th>
+                                                <th style="font-size: .9em;">LOTE</th>
+                                                <th style="font-size: .9em;">CLIENTE</th>
+                                                <th style="font-size: .9em;">CÓDIGO</th>
+                                            </tr>
+                                        </thead>
+                                    </table>
+                                    </div>
+                                </div>
+                                </div>
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php $this->load->view('template/footer_legend');?>
 </div>
 </div>
@@ -226,6 +297,10 @@
                             lblStats = '<span class="label label-primary">Donación</span>';
                         } else if (d.tipo_venta == 5) {
                             lblStats = '<span class="label label-info">Intercambio</span>';
+                        }else if(d.tipo_venta==6) {
+                            lblStats ='<span class="label label-info">Reubicación</span>';
+                        }else if(d.tipo_venta==7) {
+                            lblStats ='<span class="label label-info">Venta especial</span>';
                         }
 
                         return lblStats;
