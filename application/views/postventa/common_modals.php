58b9654def8e4d14599c7cc23470150a68ea9904
ugonzalez1512 <programador.analista26@ciudadmaderas.com>
2023-02-13 16:51:03 -0600

    Estilo en botones para solicitud

--- application/views/postventa/common_modals.php
+++ application/views/postventa/common_modals.php
@@ -802,7 +802,8 @@
             <form id="formEstatusLote" name="formEstatusLote" method="post">
                 <input type="hidden" name="id_solicitudEstatus" id="id_solicitudEstatus">
                 <div class="modal-body">
-                    <select class="select-gral m-0" title="Estatus construcción" id="construccion" name="construccion" data-style="btn" data-container="body" required></select>
+                    <select class="form-control select-const select-gral m-0" title="Estatus construcción" id="construccion" name="construccion" required>
+                    </select>
                 </div>
                 <div class="modal-footer">
                     <button type="submit" id="requestEstatusLote" class="btn btn-success btn-simple">Aceptar</button>
