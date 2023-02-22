d0926f914214e49be2ce8ecf7eb9b318fcd451d9
ozuna16 <80979535+ozuna16@users.noreply.github.com>
2023-02-22 16:26:21 -0600

    where en getEscrituracion

--- application/models/Postventa_model.php
+++ application/models/Postventa_model.php
@@ -161,17 +161,7 @@ class Postventa_model extends CI_Model
         $rol = $this->session->userdata('id_rol');
         $filtroTabla = "";
         $AddWhere = "";
-        $WhereFechas = "";
-        if($tipo_tabla == 1){
-            $filtroTabla = "AND se.id_estatus in (47,50)";
-        }else{
-            $filtroTabla = "AND se.id_estatus not in (47,50)";
-        }
-        if($begin != 0){
-          $WhereFechas = " AND se.fecha_creacion >= '$begin' AND se.fecha_creacion <= '$end' ";
-        }
-
-      
+        $WhereFechas = "";      
 
         if($estatus == 0){
         //PROPIOS
@@ -187,6 +177,15 @@ class Postventa_model extends CI_Model
         }else{
             //TODOS
             $AddWhere = " ";
+        }
+
+        if($tipo_tabla == 1){
+            $filtroTabla = $estatus == 0 ? " AND se.id_estatus in (47,50)" : " WHERE se.id_estatus in (47,50)" ;
+        }else{
+        $filtroTabla = $estatus == 0 ? " AND se.id_estatus not in (47,50)" : " WHERE se.id_estatus not in (47,50)";
+        }
+        if($begin != 0){
+        $WhereFechas = " AND se.fecha_creacion >= '$begin' AND se.fecha_creacion <= '$end' ";
         }
         
         return $this->db->query("SELECT distinct(se.id_solicitud),se.id_titulacion, se.valor_contrato, se.id_estatus, se.fecha_creacion, l.nombreLote, cond.nombre nombreCondominio, r.nombreResidencial, CONCAT(c.nombre, ' ', c.apellido_paterno, ' ', c.apellido_materno) as cliente, n.pertenece, se.bandera_notaria, se.descuento, se.aportacion, ar.id_opcion as id_area, (CASE WHEN se.id_estatus IN (4,2,3) AND (se.bandera_admin IS NULL OR se.bandera_comite IS NULL) THEN 'Administración / Comité técnico' ELSE ar.nombre END) area, cp.area_actual, dc.expediente, dc.tipo_documento, dc.idDocumento, cr.area_sig, CONCAT(cp.clave_actividad ,' - ', ae.nombre) AS nombre_estatus, cr.estatus_siguiente, cr.nombre_estatus_siguiente, cr.tipo_permiso, se.bandera_comite, se.bandera_admin, se.estatus_construccion, se.nombre_a_escriturar, se.cliente_anterior, (CASE when cp.tipo_permiso = 3 THEN 'RECHAZO' ELSE '' END ) rechazo, concat((select[dbo].[DiasLaborales]( (dateadd(day,1,se.fecha_modificacion)) ,GETDATE())), ' día(s) de ',ae.dias_vencimiento) vencimiento, de4.contrato,pr.banderaPresupuesto,presup2.presupuestoAprobado,se.id_notaria, se.fecha_firma, a.descripcion ultimo_comentario,CONCAT(userAsig.nombre, ' ', userAsig.apellido_paterno, ' ', userAsig.apellido_materno) asignada_a,de2.documentosCargados, de2.estatusValidacion,de2.no_rechazos
