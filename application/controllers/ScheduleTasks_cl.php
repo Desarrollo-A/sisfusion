0a06d952de224313f65977cc92a8c002e885e054
mgarmarcm <mariadejesus.garduno@ciudadmaderas.com>
2023-03-31 11:29:47 -0600

    Ajustes generales
    ScheduleTasks_cl.php: se modifica cuerpo del correo que se envía para el cambio mensual de la contraseña del asesor comodín.
    
    Reporte_model.php: se agregó la función TRYCAST() cuando se trabaje con ds.costom2f para castearlo a un DECIMAL a fin de poder realizar operaciones con este valor

--- application/controllers/ScheduleTasks_cl.php
+++ application/controllers/ScheduleTasks_cl.php
@@ -1512,7 +1512,7 @@ public function select_gph_maderas_64(){ //HACER INSERT DE LOS LOTES EN 0 Y PASA
           
           <p style='padding: 10px 90px;text-align: center;'>La nueva contraseña del usuario ASESOR COMODÍN es: $key
           </p>
-          <p style='padding: 10px 90px;text-align: center;'><b>*Recuerda que esta contraseña solo es válida durante 15 días.</b> 
+          <p style='padding: 10px 90px;text-align: center;'><b>*Recuerda que esta contraseña sólo es válida durante 1 mes.</b> 
           </p>
       </td>
     </tr></table>
