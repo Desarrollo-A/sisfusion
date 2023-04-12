ac7a9b6af99d3cf790ac2ebac4c5eecfe3d03c81
programador21D <programador.analista21@ciudadmaderas.com>
2023-04-12 12:20:57 -0600

    cambio de nombres

--- application/views/ventas/dispersar_pago_neodata.php
+++ application/views/ventas/dispersar_pago_neodata.php
@@ -532,7 +532,7 @@
     var url2 = "<?=base_url()?>index.php/";
 
     </script>
-    <script src="<?= base_url() ?>dist/js/controllers/comisiones/despersar_pago_neodata.js"></script>
+    <script src="<?= base_url() ?>dist/js/controllers/comisiones/dispersar_pago_neodata.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.flash.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
