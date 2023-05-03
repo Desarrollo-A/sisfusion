fb03e7f12e87028f0cc2fb0b13c13b0802bf4bf1
programador21D <programador.analista21@ciudadmaderas.com>
2023-05-03 09:48:28 -0600

    cambio de nombre en function

--- application/controllers/Comisiones.php
+++ application/controllers/Comisiones.php
@@ -6448,7 +6448,7 @@ for ($d=0; $d <count($dos) ; $d++) {
 
  
     
-    public function viewDetenidas() {
+    public function detenidas() {
       $datos = array();
       $datos["datos2"] = $this->Asesor_model->getMenu($this->session->userdata('id_rol'))->result();
       $datos["datos3"] = $this->Asesor_model->getMenuHijos($this->session->userdata('id_rol'))->result();
