3b0681faf2f2d6ccfbe47eaae8e458d5c3cdfd93
programador21D <programador.analista21@ciudadmaderas.com>
2023-05-03 09:07:01 -0600

    recuperación de controversias

--- application/models/Comisiones_model.php
+++ application/models/Comisiones_model.php
@@ -8059,13 +8059,14 @@ public function getDataDispersionPagoEspecial($val = '') {
         $query = $this->db->query("SELECT DISTINCT(l.idLote), res.nombreResidencial, cond.nombre as nombreCondominio,
         l.nombreLote, l.tipo_venta, vc.id_cliente AS compartida, l.idStatusContratacion,
         hl.motivo, hl.comentario
-
+        ,oxc.nombre as motivoOpc
         FROM lotes l 
         INNER JOIN clientes cl ON cl.id_cliente = l.idCliente AND cl.status = 1 
         INNER JOIN condominios cond ON l.idCondominio=cond.idCondominio 
         INNER JOIN residenciales res ON cond.idResidencial = res.idResidencial
         INNER JOIN historial_log hl ON hl.identificador = l.idLote AND hl.tabla = 'pago_comision' AND hl.estatus = 1
         LEFT JOIN ventas_compartidas vc ON vc.id_cliente = cl.id_cliente AND vc.estatus = 1
+        LEFT JOIN opcs_x_cats oxc ON oxc.id_catalogo = 88 and oxc.id_opcion = TRY_CAST( hl.motivo AS BIGINT)
         WHERE l.idStatusContratacion BETWEEN 9 AND 15 
         AND l.status = 1 
         AND l.registro_comision in (10,11,18)
--- application/views/ventas/active_commissions.php
+++ application/views/ventas/active_commissions.php
@@ -278,13 +278,8 @@
     </div>
     </div><!--main-panel close-->
     <?php $this->load->view('template/footer'); ?>
-    <script > 
-    var url = "<?=base_url()?>";
-    var url2 = "<?=base_url()?>index.php/";
-
-    </script>
     <!--DATATABLE BUTTONS DATA EXPORT-->
-    <script src="<?= base_url() ?>dist/js/controllers/comisiones/active_commissions.js"></script>
+    <script src="<?= base_url() ?>dist/js/controllers/comisiones/activas.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.flash.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
