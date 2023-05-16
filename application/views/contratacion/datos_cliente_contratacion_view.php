bf72470a79b0461307d24d7c14f8869cd5e59675
danteAldair <programador.analista18@ciudadmaderas.com>
2023-05-16 15:57:52 -0600

    Cambios Dante Aldair /* * Cambio de nombre de los JS nuevos, se quito el "view" dentro del nombre del archivo. */

--- application/views/contratacion/datos_cliente_contratacion_view.php
+++ application/views/contratacion/datos_cliente_contratacion_view.php
@@ -180,5 +180,5 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
-    <script src="<?=base_url()?>dist/js/controllers/contratacion/datos_cliente_contratacion_view.js"></script>
+    <script src="<?=base_url()?>dist/js/controllers/contratacion/datos_cliente_contratacion.js"></script>
 </body>
--- application/views/ventasAsistentes/datos_cliente_contrato_ventasAsistentes_view.php
+++ application/views/ventasAsistentes/datos_cliente_contrato_ventasAsistentes_view.php
@@ -227,5 +227,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
-<script src="<?=base_url()?>dist/js/controllers/clientes/datos_cliente_contrato_ventasAsistentes_view.js"></script>
+<script src="<?=base_url()?>dist/js/controllers/clientes/datos_cliente_contrato_ventasAsistentes.js"></script>
 
