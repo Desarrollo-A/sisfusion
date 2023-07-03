a20ffcdb08e6cd8c70a4e5b9c99143065e27a80a
JosueJaimeB <134556235+JosueJaimeB@users.noreply.github.com>
2023-07-03 13:12:50 -0600

    prueba

--- application/views/usuarios/users_list.php
+++ application/views/usuarios/users_list.php
@@ -361,10 +361,6 @@
     </body>
 
     <?php $this->load->view('template/footer'); ?>
-
-    <script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
     <script src="<?= base_url() ?>dist/js/controllers/usuarios-1.1.0.js"></script>
     
     </html>
