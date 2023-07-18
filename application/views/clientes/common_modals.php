29a9845739bfa59ab0f2410b1e117ae3c1fb4d2e
JosueJaimeB <134556235+JosueJaimeB@users.noreply.github.com>
2023-07-18 16:22:12 -0600

    Correción consulta prospecto

--- application/views/clientes/common_modals.php
+++ application/views/clientes/common_modals.php
@@ -693,110 +693,50 @@
     </div>
 </div>
 <!-- MODAL-->
-    <div class="modal fade" id="seeInformationModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
-        <div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
-                        <i class="material-icons" onclick="cleanComments()">clear</i>
-                    </button>
-                    <h4 class="modal-title">Consulta información</h4>
-                </div>
-                <div class="modal-body">
-                    <div role="tabpanel">
-                        <!-- Nav tabs -->
-                        <ul class="nav nav-tabs" role="tablist" style="background: #003d82;">
-                            <li role="presentation" class="active"><a href="#generalTab" aria-controls="generalTab" role="tab" data-toggle="tab">General</a></li>
-                            <li role="presentation"><a href="#commentsTab" aria-controls="commentsTab" role="tab" data-toggle="tab">Comentarios</a></li>
-                            <li role="presentation"><a href="#changelogTab" aria-controls="changelogTab" role="tab" data-toggle="tab">Bitácora de cambios</a></li>
-                        </ul>
-                        <!-- Tab panes -->
-                        <div class="tab-content">
-                            <div role="tabpanel" class="tab-pane active" id="generalTab">
-                                <div class="row">
-                                    <div class="col-sm-3">
-                                        <div class="form-group">
-                                            <label class="control-label">Personalidad jurídica</label>
-                                            <input id="legal-personality-lbl" type="text" class="form-control input-gral" disabled>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-3">
-                                        <div class="form-group">
-                                            <label class="control-label">Nacionalidad</label>
-                                            <input id="nationality-lbl" type="text" class="form-control input-gral" disabled>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-3">
-                                        <div class="form-group">
-                                            <label class="control-label">CURP</label>
-                                            <input id="curp-lbl" type="text" class="form-control input-gral" disabled>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-3">
-                                        <div class="form-group">
-                                            <label class="control-label">RFC</label>
-                                            <input id="rfc-lbl" type="text" class="form-control input-gral" disabled>
-                                        </div>
+<div class="modal fade" id="seeInformationModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                    <i class="material-icons" onclick="cleanComments()">clear</i>
+                </button>
+                <h4 class="modal-title">Consulta información</h4>
+            </div>
+            <div class="modal-body">
+                <div role="tabpanel">
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-tabs" role="tablist" style="background: #003d82;">
+                        <li role="presentation" class="active"><a href="#generalTab" aria-controls="generalTab" role="tab" data-toggle="tab">General</a></li>
+                        <li role="presentation"><a href="#commentsTab" aria-controls="commentsTab" role="tab" data-toggle="tab">Comentarios</a></li>
+                        <li role="presentation"><a href="#changelogTab" aria-controls="changelogTab" role="tab" data-toggle="tab">Bitácora de cambios</a></li>
+                    </ul>
+                    <!-- Tab panes -->
+                    <div class="tab-content">
+                        <div role="tabpanel" class="tab-pane active" id="generalTab">
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <div class="form-group">
+                                        <label class="control-label">Personalidad jurídica</label>
+                                        <input id="legal-personality-lbl" type="text" class="form-control input-gral" disabled>
                                     </div>
                                 </div>
-                                <!-- recovered-->
-
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <div class="form-group">
-                                            <label class="control-label">Nombre / Razón social</label>
-                                            <input id="name-lbl" type="text" class="form-control input-gral" disabled>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-4">
-                                        <div class="form-group">
-                                            <label class="control-label">Correo electrónico</label>
-                                            <input id="email-lbl" type="text" class="form-control input-gral" disabled>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-2">
-                                        <div class="form-group">
-                                            <label class="control-label">Teléfono</label>
-                                            <input id="phone-number-lbl" type="text" class="form-control input-gral" disabled>
-                                        </div>
+                                <div class="col-sm-3">
+                                    <div class="form-group">
+                                        <label class="control-label">Nacionalidad</label>
+                                        <input id="nationality-lbl" type="text" class="form-control input-gral" disabled>
                                     </div>
                                 </div>
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <div class="form-group">
-                                            <label class="control-label">¿Cómo nos contactaste?</label>
-                                            <input id="prospecting-place-lbl" type="text" class="form-control input-gral" disabled>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <div class="form-group">
-                                            <label class="control-label">Plaza de venta</label>
-                                            <input id="sales-plaza-lbl" type="text" class="form-control input-gral" disabled>
-                                        </div>
+                                <div class="col-sm-3">
+                                    <div class="form-group">
+                                        <label class="control-label">CURP</label>
+                                        <input id="curp-lbl" type="text" class="form-control input-gral" disabled>
                                     </div>
                                 </div>
-                                <div class="row">
-                                    <div class="col-sm-4">
-                                        <div class="form-group">
-                                            <label class="control-label">Asesor</label>
-                                            <input id="asesor-lbl" type="text" class="form-control input-gral" disabled>
-                                        </div>
+                                <div class="col-sm-3">
+                                    <div class="form-group">
+                                        <label class="control-label">RFC</label>
+                                        <input id="rfc-lbl" type="text" class="form-control input-gral" disabled>
                                     </div>
-                                    <div class="col-sm-4">
-                                        <div class="form-group">
-                                            <label class="control-label">Coordinador</label>
-                                            <input id="coordinador-lbl" type="text" class="form-control input-gral" disabled>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-4">
-                                        <div class="form-group">
-                                            <label class="control-label">Gerente</label>
-                                            <input id="gerente-lbl" type="text" class="form-control input-gral" disabled>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <input type="hidden" id="id-prospecto-lbl" name="id_prospecto_lbl">
                                 </div>
                             </div>
 							<!-- recovered-->
@@ -869,28 +809,29 @@
                                     </div>
                                 </div>
                             </div>
-                            <div role="tabpanel" class="tab-pane" id="changelogTab">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="card card-plain">
-                                            <div class="card-content">
-                                                <ul class="timeline-3" id="changelog"></ul>
-                                            </div>
+                        </div>
+                        <div role="tabpanel" class="tab-pane" id="changelogTab">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="card card-plain">
+                                        <div class="card-content">
+                                            <ul class="timeline-3" id="changelog"></ul>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <input type="hidden" name="prospecto_lbl" id="prospecto_lbl">
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger btn-simple" data-dismiss="modal" onclick="cleanComments()">Cerrar</button>
-                    <button type="button" class="btn btn-primary" onclick="{{$('#prospecting-place-lbl').val() == 'MKT digital (especificar)' ? printProspectInfoMktd() : printProspectInfo()}}"><i class="material-icons">cloud_download</i> Descargar pdf</button>
-                </div>
+                <input type="hidden" name="prospecto_lbl" id="prospecto_lbl">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger btn-simple" data-dismiss="modal" onclick="cleanComments()">Cerrar</button>
+                <button type="button" class="btn btn-primary" onclick="{{$('#prospecting-place-lbl').val() == 'MKT digital (especificar)' ? printProspectInfoMktd() : printProspectInfo()}}"><i class="material-icons">cloud_download</i> Descargar pdf</button>
             </div>
         </div>
     </div>
+</div>
 <!-- END MODAL-->
 
 <div class="modal fade" id="agendaInsert" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
