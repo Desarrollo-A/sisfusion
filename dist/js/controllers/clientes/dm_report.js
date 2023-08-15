0613d16ded47b2c2741a9f722a982c4dd13c395a
ugonzalez1512 <programador.analista26@ciudadmaderas.com>
2023-08-15 16:28:48 -0600

    MÓDULO REPORTES Y COMISIONES PARA EL ROL DE ASISTENTE GERENTE

--- dist/js/controllers/clientes/dm_report.js
+++ dist/js/controllers/clientes/dm_report.js
@@ -59,8 +59,7 @@ function fillTable(typeTransaction, beginDate, endDate, where) {
             title:'Reporte de estatus por prospecto',
             exportOptions: {
                 columns: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-                format: 
-                {
+                format: {
                     header:  function (d, columnIdx) {
                         return ' ' + titulos_intxt[columnIdx] + ' ';
                     }
