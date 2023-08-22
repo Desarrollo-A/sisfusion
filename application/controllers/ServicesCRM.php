833f3248aaa044f2e62fb3bbffcfbabd32d19016
mgarmarcm <mariadejesus.garduno@ciudadmaderas.com>
2023-08-21 21:51:18 -0600

    Update ServicesCRM.php

--- application/controllers/ServicesCRM.php
+++ application/controllers/ServicesCRM.php
@@ -41,6 +41,9 @@ class ServicesCRM extends CI_Controller {
 
     function codificarTest() {
         $miJson = array(
+            /*"id_usuario" => 5425,
+            "estatus" => 1,
+            "modificado_por" => 1*/
             "rfc" => "HGDE350247FF8",
             "id_rol" => 7,
             "id_lider" => 655,
