a903b604cfbe87f7cc5eba2aebda18be55c1536b
programador21D <programador.analista21@ciudadmaderas.com>
2023-09-05 09:19:25 -0600

    arrchivoExc

--- dist/js/controllers/comisiones/dispersion.js
+++ dist/js/controllers/comisiones/dispersion.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
             titleAttr: 'DESCARGAR ARCHIVO DE EXCEL',
             title: 'Dispersión de pago',
             exportOptions: {
-                columns: [1, 2, 3, 4, 5, 6, 7, 8,9,10],
+                columns: [1, 2, 3, 4, 5, 6, 7, 8,9,10,11],
                 format: {
                     header: function (d, columnIdx) {
                         return ' ' + titulos_intxt[columnIdx] + ' ';
