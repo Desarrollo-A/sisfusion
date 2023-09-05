035e0fcd0120f29f673caffbf24a8e4da1889afa
programador21D <programador.analista21@ciudadmaderas.com>
2023-09-05 09:11:02 -0600

    error  Ticket #62510 fecha ultima dispersion

--- dist/js/controllers/comisiones/dispersion.js
+++ dist/js/controllers/comisiones/dispersion.js
@@ -132,6 +132,18 @@ $(document).ready(function () {
                 return fechaNeodata+rescisionLote;
             }},
             { data: function (d) {
+                var ultima_dispersion;
+
+                if( d.ultima_dispersion == null ) {
+                    ultima_dispersion ='<span class="label lbl-gray">Sin Definir</span>';
+                }else {
+                    ultima_dispersion = '<br><span class="label lbl-lightBlue">'+d.ultima_dispersion+'</span>';
+                }
+                
+                return ultima_dispersion;
+            }},
+            
+            { data: function (d) {
                 var BtnStats = '';
                 var RegresaActiva = '';
                 if(d.penalizacion == 1 && d.bandera_penalizacion == 0 && d.id_porcentaje_penalizacion != '4') {
