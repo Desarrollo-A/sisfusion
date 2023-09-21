47f2a7dea30be0418b77fe22bf3286e4d5d921a9
jcoronelg <programador.analista24@ciudadmaderas.com>
2023-09-21 11:52:03 -0600

    Se cambia la forma de obtener el valor de TotalNeto en el estatus 5

--- application/controllers/Contraloria.php
+++ application/controllers/Contraloria.php
@@ -12,7 +12,6 @@ class Contraloria extends CI_Controller {
         $this->load->helper(array('url','form'));
         $this->load->database('default');
         $this->load->library('email');
-        $this->load->model('Reestructura_model');
         $this->validateSession();
         date_default_timezone_set('America/Mexico_City');
         $val =  $this->session->userdata('certificado'). $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
@@ -214,17 +213,6 @@ class Contraloria extends CI_Controller {
     public function getregistroStatus13ContratacionContraloria() {
         $datos = array();
         $datos = $this->Contraloria_model->registroStatusContratacion13();
-
-        if($datos != null) {
-            echo json_encode($datos);
-        } else {
-            echo json_encode(array());
-        }
-
-    }
-
-    public function obtenerLoteAnteriorPorIdClienteNuevo($id_cliente) {
-        $datos = $this->Reestructura_model->buscarLoteAnteriorPorIdClienteNuevo($id_cliente);
 
         if($datos != null) {
             echo json_encode($datos);
