bd1b2df05d23a79f5363e172c95af1bcb1588e99
tino1243 <145475594+tino1243@users.noreply.github.com>
2023-10-03 13:38:36 -0600

    Cambio de RL y  Baja de venta particular
    Una nueva vista para poder cambiar al representante legal y Reporte de lotes bloqueados se agrego un botón para remover la venta particular

--- application/controllers/Contraloria.php
+++ application/controllers/Contraloria.php
@@ -133,6 +133,11 @@ class Contraloria extends CI_Controller {
         $this->load->view("contratacion/datos_lote_contratacion_view", $datos);
     }
 
+    public function cambioRL() {
+        $this->load->view('template/header');
+        $this->load->view("contraloria/cambioRL_view");
+    }
+
     public function lista_proyecto() {
         echo json_encode($this->Contraloria_model->get_proyecto_lista()->result_array());
     }
@@ -3117,5 +3122,67 @@ class Contraloria extends CI_Controller {
     {
         $data = $this->Contraloria_model->getAllDsByLider($this->session->userdata('id_lider'));
         echo json_encode($data);
+    }
+
+    public function getCambioRLContraloria($idLote) {
+        $datos = array();
+        $datos = $this->Contraloria_model->getCambioRL($idLote);
+        if($datos != null) {
+            echo json_encode($datos);
+        } else {
+            echo json_encode(array());
+        }
+    }
+
+    public function selectRL() {
+        echo json_encode($this->Contraloria_model->getCatalogsRL()->result_array());
+    }
+
+    public function selectSede() {
+        echo json_encode($this->Contraloria_model->getSedeRl()->result_array());
+    }
+
+    public function selectStatusLote() {
+        echo json_encode($this->Contraloria_model->getStatusLoteRl()->result_array());
+    }
+
+    public function updateRL()
+    {
+   
+        $idLote=$this->input->post('idLote');
+        $idCliente=$this->input->post('idCliente');
+        $rl = $this->input->post('rl');
+
+        $dataToUpdate = array("rl"=> $rl, "modificado_por" => $this->session->userdata('id_usuario'));
+        $responseUpdate = $this->General_model->updateRecord("clientes", $dataToUpdate, "idLote", $idLote);
+
+            $data['message'] = 'OK';
+            echo json_encode($data);
+    }
+
+    public function updateSede()
+    {
+        $idSede=$this->input->post('sede');
+        $id_usuario = $this->session->userdata('id_usuario');
+        $idLote=$this->input->post('idLote');
+
+        $dataToUpdate = array("ubicacion"=> $idSede, "usuario" => $this->session->userdata('id_usuario'));
+        $responseUpdate = $this->General_model->updateRecord("lotes", $dataToUpdate, "idLote", $idLote);
+
+            $data['message'] = 'OK';
+            echo json_encode($data);
+    }
+
+    public function updateStatusLote()
+    {
+        $idEstatuslote=$this->input->post('lote');
+        $id_usuario = $this->session->userdata('id_usuario');
+        $idLote=$this->input->post('idLote');
+
+        $dataToUpdate = array("idStatusLote"=> $idEstatuslote, "usuario" => $this->session->userdata('id_usuario'));
+        $responseUpdate = $this->General_model->updateRecord("lotes", $dataToUpdate, "idLote", $idLote);
+
+            $data['message'] = 'OK';
+            echo json_encode($data);
     }
 }
