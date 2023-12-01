aec76e9174982036c93d46f17cd9e70d310fbdee
programador21D <programador.analista21@ciudadmaderas.com>
2023-12-01 16:30:41 -0600

    pagos_ooam vista

--- application/views/pagos/revision_asimilados_view.php
+++ application/views/pagos/revision_asimilados_view.php
@@ -47,7 +47,7 @@
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                     <ul class="nav nav-tabs nav-tabs-cm" role="tablist">
                         <li class="active"><a href="#asimiladosComercializacion" role="tab" data-toggle="tab">Asimilados lotes</a></li>
-                        <li><a href="#asimiladosOOAM" role="tab" data-toggle="tab">Asimilados ooam</a></li>
+                        <!-- <li><a href="#asimiladosOOAM" role="tab" data-toggle="tab">Asimilados ooam</a></li> -->
                     </ul>
             
                     <div class="card no-shadow m-0 border-conntent__tabs">
@@ -196,5 +196,5 @@
     <?php $this->load->view('template/footer');?>
     <script src="<?= base_url() ?>dist/js/core/modal-general.js"></script>
     <script src="<?= base_url() ?>dist/js/controllers/pagos/revision_asimilados.js"></script>
-    <script src="<?= base_url() ?>dist/js/controllers/ooam/revision_asimilados_ooam.js"></script>
+    <!-- <script src="<?= base_url() ?>dist/js/controllers/ooam/revision_asimilados_ooam.js"></script> -->
 </body>
--- application/views/pagos/revision_factura_view.php
+++ application/views/pagos/revision_factura_view.php
@@ -47,7 +47,7 @@
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                     <ul class="nav nav-tabs nav-tabs-cm" role="tablist">
                         <li class="active"><a href="#facturasComercializacion" role="tab" data-toggle="tab">Facturas lotes</a></li>
-                        <li><a href="#facturasOOAM" role="tab" data-toggle="tab">Facturas ooam</a></li>
+                        <!-- <li><a href="#facturasOOAM" role="tab" data-toggle="tab">Facturas ooam</a></li> -->
                     </ul>
             
                     <div class="card no-shadow m-0 border-conntent__tabs">
@@ -192,5 +192,5 @@
     <?php $this->load->view('template/footer');?>
     <script src="<?= base_url() ?>dist/js/core/modal-general.js"></script>
     <script src="<?= base_url() ?>dist/js/controllers/pagos/revision_factura.js"></script>
-    <script src="<?= base_url() ?>dist/js/controllers/ooam/revision_factura_ooam.js"></script>
+    <!-- <script src="<?= base_url() ?>dist/js/controllers/ooam/revision_factura_ooam.js"></script> -->
 </body>
