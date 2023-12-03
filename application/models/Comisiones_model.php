c4263c8f6c179696bea7f085af93e50f2dbbd253
Kelyn Rodríguez <90913597+Kelyn24@users.noreply.github.com>
2023-12-03 17:50:47 -0600

    filtros funciones liquidadas y activas

--- application/models/Comisiones_model.php
+++ application/models/Comisiones_model.php
@@ -224,9 +224,7 @@ class Comisiones_model extends CI_Model {
         LEFT JOIN (SELECT COUNT(*) liquidada2, id_lote FROM comisiones WHERE ooam = 2 GROUP BY id_lote) liq2 ON liq2.id_lote = l.idLote
         LEFT JOIN (SELECT COUNT(*) reubicadas, idCliente FROM comisionesReubicadas GROUP BY idCliente) reub ON reub.idCliente = clr.id_cliente
         LEFT JOIN (SELECT COUNT(*) dispersar, id_lote FROM comisiones WHERE ooam = 1 GROUP BY id_lote) ooamDis ON ooamDis.id_lote = l.idLote
-        WHERE 
-        l.idLote IN (71723,80643,10,51,152,165,11010,17310)
-        /*(l.idLote IN (7167,7168,10304,17231,18338,18549,23730,27250) 
+        WHERE l.idLote IN (7167,7168,10304,17231,18338,18549,23730,27250) 
         AND l.registro_comision not IN (7) 
         AND pc.bandera IN (0,100)) 
         AND cl.proceso IN (0)
@@ -241,7 +239,6 @@ class Comisiones_model extends CI_Model {
         AND cl.fechaApartado >= '2020-03-01' 
         AND (cl.id_subdirector IS NOT NULL AND cl.id_subdirector != '' AND cl.id_subdirector != 0 )
         AND ISNULL(l.totalNeto2, 0) > 0)
-        */
         ORDER BY l.idLote"); 
         return $query;
     }
@@ -1070,10 +1067,8 @@ class Comisiones_model extends CI_Model {
         LEFT JOIN (SELECT COUNT(*) dispersar, id_lote FROM comisiones WHERE ooam = 1 GROUP BY id_lote) ooamDis ON ooamDis.id_lote = l.idLote
         LEFT JOIN (SELECT COUNT(*) total, id_lote FROM comisiones WHERE ooam = 1 GROUP BY id_lote) ooam ON ooam.id_lote = l.idLote
         LEFT JOIN (SELECT COUNT(*) total, id_lote FROM comisiones WHERE ooam = 2 GROUP BY id_lote) ventas ON ventas.id_lote = l.idLote
-        WHERE 
-        l.idLote IN (71723,80643,10,51,152,165,11010,17310)
-        /*WHERE l.idStatusContratacion BETWEEN 11 AND 15 AND cl.status = 1 AND l.status IN (0,1) AND l.registro_comision IN (7) AND l.tipo_venta IS NOT NULL AND l.tipo_venta IN (1,2,7)
-        ORDER BY l.idLote*/");
+        WHERE l.idStatusContratacion BETWEEN 11 AND 15 AND cl.status = 1 AND l.status IN (0,1) AND l.registro_comision IN (7) AND l.tipo_venta IS NOT NULL AND l.tipo_venta IN (1,2,7)
+        ORDER BY l.idLote");
         return $query ;
     }
     
