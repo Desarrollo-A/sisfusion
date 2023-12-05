1aafcf6394461f15034bef8f77f5ad9eb987c6ca
ecc718 <programador.analista8@ciudadmaderas.com>
2023-12-05 13:36:05 -0600

    Actualización para el correcto funcionamiento de copy V2

--- dist/js/controllers/reestructura/reubicacionClientes.js
+++ dist/js/controllers/reestructura/reubicacionClientes.js
@@ -1395,8 +1395,6 @@ const botonesAccionReubicacion = (d) => {
     if (idEstatusPreproceso === 2 && totalContrato === totalContratoRef && FLAGPROCESOJURIDICO === 0) { //subiendo contratos //&& FLAGPROCESOJURIDICO === 0  //aun no es el cambio final se comenta para seguir con el proceso
         editar = 1;
         btnShow = 'fa-edit';
-        btnContratoFirmado = 'fa-eye';
-        tooltipCF = 'VER CONTRATO FIRMADO';
     }
     if(d.idContratoFirmado != null){
         btnContratoFirmado = 'fa-eye';
