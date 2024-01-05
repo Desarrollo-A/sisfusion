b805b6c2d7ca92c18254484e69f64d9125355c64
ecc718 <programador.analista8@ciudadmaderas.com>
2024-01-05 09:09:19 -0600

    Ajustes CRM

--- application/models/Reestructura_model.php
+++ application/models/Reestructura_model.php
@@ -663,7 +663,7 @@ class Reestructura_model extends CI_Model
         INNER JOIN opcs_x_cats oxc1 ON oxc1.id_opcion = lo.estatus_preproceso AND oxc1.id_catalogo = 106
         LEFT JOIN usuarios u6 ON u6.id_usuario = id_usuario_asignado
         LEFT JOIN lotesFusion lf ON lf.idLote = lo.idLote
-        WHERE lo.liberaBandera = 1 AND lo.status = 1 AND ISNULL(lo.tipo_venta, 0) != 1 AND id_usuario_asignado = 0 $filtroSede")->result_array();
+        WHERE lo.liberaBandera = 1 AND lo.status = 1 AND ISNULL(lo.tipo_venta, 0) != 1 AND lo.estatus_preproceso IN(1, 0) $filtroSede")->result_array();
     }
 
     public function getListaUsuariosParaAsignacion() {
