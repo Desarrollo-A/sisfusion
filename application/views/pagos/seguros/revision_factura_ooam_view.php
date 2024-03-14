33b853f9365d546593ae48425923ff8bf6735ae3
OmarMolinaC98 <programador.analista35@ciudadmaderas.com>
2024-03-14 10:59:52 -0600

    creación vistas: dispersión, activas y especiales

---
+++ application/views/pagos/seguros/revision_factura_ooam_view.php
@@ -0,0 +1,60 @@
+<div class="tab-pane" id="facturaOOAM">
+    <div class="text-center">
+        <h3 class="card-title center-align">Comisiones en revisión <b>factura OOAM</b></h3>
+        <p class="card-title pl-1">Comisiones solicitadas por el área de OOAM para proceder a pago en esquema de factura.</p>
+    </div>
+    <div class="toolbar">
+        <div class="container-fluid p-0">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                    <div class="form-group d-flex justify-center align-center">
+                        <h4 class="title-tot center-align m-0">Disponible:</h4>
+                        <p class="input-tot pl-1" name="disponibleFacturaOOAM" id="disponibleFacturaOOAM">$0.00</p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                    <div class="form-group d-flex justify-center align-center">
+                        <h4 class="title-tot center-align m-0">Autorizar:</h4>
+                        <p class="input-tot pl-1" name="autorizarFacturaOOAM" id="autorizarFacturaOOAM">$0.00</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row aligned-row d-flex align-end">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">          
+                <div class="form-group">
+                    <label class="control-label" for="proyectoFacturaOOAM">Proyecto</label>
+                    <select name="proyectoFacturaOOAM" id="proyectoFacturaOOAM" class="selectpicker select-gral m-0" data-style="btn " data-show-subtext="true" data-live-search="true" data-container="body" title="SELECCIONA UNA OPCIÓN" data-size="7" required></select>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6"> 
+                <div class="form-group">
+                    <label class="control-label" for="condominioFacturaOOAM">Condominio</label>
+                    <select class="selectpicker select-gral m-0" id="condominioFacturaOOAM" name="condominioFacturaOOAM[]" data-style="btn " data-show-subtext="true" data-live-search="true" data-container="body" title="SELECCIONA UNA OPCIÓN" data-size="7" required></select>
+                </div>
+            </div>
+        </div>
+        <table class="table-striped table-hover" id="tabla_factura_ooam" name="tabla_factura_ooam">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>ID PAGO</th>
+                    <th>PROYECTO</th>
+                    <th>CONDOMINIO</th>
+                    <th>LOTE</th>
+                    <th>REFERENCIA</th>
+                    <th>PRECIO DEL LOTE</th>
+                    <th>EMPRESA</th>
+                    <th>TOTAL DE LA COMISIÓN</th>
+                    <th>PAGADO POR EL CLIENTE</th>
+                    <th>TOTAL A PAGAR</th>
+                    <th>TIPO DE VENTA</th>
+                    <th>USUARIO</th>
+                    <th>PUESTO</th>
+                    <th>FECHA DE ENVÍO</th>
+                    <th>ACCIONES</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</div>
