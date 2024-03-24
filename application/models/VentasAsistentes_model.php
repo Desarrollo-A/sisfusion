a66cf65b865cfcc3345d1da0df9d2dfe1f7a03a4
mgarmarcm <mariadejesus.garduno@ciudadmaderas.com>
2024-03-23 20:50:48 -0600

    Permisos especiales asistentes de gerencia

--- application/models/VentasAsistentes_model.php
+++ application/models/VentasAsistentes_model.php
@@ -354,7 +354,7 @@ class VentasAsistentes_model extends CI_Model {
                     $filtroGerente = "AND cl.id_gerente IN ($id_lider, 694, 4509";
                     $filtroSede = "";
 				} else if ($id_usuario == 14952) { // GUILLERMO HELI IZQUIERDO VIEYRA
-                    $filtroGerente = "AND cl.id_gerente IN ($id_lider, 694, 674)";
+                    $filtroGerente = "AND cl.id_gerente IN ($id_lider, 13295)";
                     $filtroSede = "";
 				} else if ($id_usuario == 12292) { // REYNALDO HERNÁNDEZ SANCHEZ
                     $filtroGerente = "AND cl.id_gerente IN ($id_lider, 3111)";
