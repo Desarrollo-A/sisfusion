afc45a38afc81cd83858bf30365f16b47e6b31a4
OmarMolinaC98 <programador.analista35@ciudadmaderas.com>
2024-04-09 15:45:42 -0600

    condicion de 3 meses en universidad

---
+++ application/views/universidad/conglomerado_cards_view.php
@@ -0,0 +1,67 @@
+<div class="card-content">
+    <div class="row">
+        <div class="col-xl-3 col-md-3 mb-3" style="padding-top:35px; margin-bottom:2px">
+            <div class="card border shadow py-2">
+                <div class="card-body">
+                    <div class="no-gutters align-items-center">
+                        <div class="col mr-12">
+                            <div class="text-xs font-weight-bold lbl-sky text-uppercase center-align"><p>Total:</p></div>
+                            <div class="center-align" style="padding-top:20px; margin-bottom:2px;">
+                                <i class="fas fa-coins fa-2x" style="color: #0067d4;"></i>
+                            </div>
+                            <div id="totalGeneral" class="h5 totalp mb-0 font-weight-bold text-gray-800 center-align">$0</div>
+                        </div>            
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-3 mb-3" style="padding-top:35px; margin-bottom:2px">
+            <div class="card border shadow py-2">
+                <div class="card-body">
+                    <div class="no-gutters align-items-center">
+                        <div class="col mr-12">
+                            <div class="text-xs font-weight-bold lbl-sky text-uppercase center-align">Total recaudado:</div>
+                            <div class="center-align" style="padding-top:20px; margin-bottom:2px;">
+                                <i class="fas fa-coins fa-2x" style="color: #0067d4;"></i>
+                            </div>
+                            <div id="totalRecaudado" class="h5 totalp mb-0 font-weight-bold text-gray-800 center-align">$0</div>
+                        </div>            
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-3 mb-3" style="padding-top:35px; margin-bottom:2px">
+            <div class="card border shadow py-2">
+                <div class="card-body">
+                    <div class="no-gutters align-items-center">
+                        <div class="col mr-12">
+                            <div class="text-xs font-weight-bold lbl-sky text-uppercase center-align">Total pagado caja:</div>
+                            <div class="center-align" style="padding-top:20px; margin-bottom:2px;">
+                                <i class="fas fa-coins fa-2x" style="color: #0067d4;"></i>
+                            </div>
+                            <div id="totalPagadoCaja" class="h5 totalp mb-0 font-weight-bold text-gray-800 center-align">$0</div>
+                        </div>            
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-3 mb-3" style="padding-top:35px; margin-bottom:2px">
+            <div class="card border shadow py-2">
+                <div class="card-body">
+                    <div class="no-gutters align-items-center">
+                        <div class="col mr-12">
+                            <div class="text-xs font-weight-bold lbl-sky text-uppercase center-align">Pendiente:</div>
+                            <div class="center-align" style="padding-top:20px; margin-bottom:2px;">
+                                <i class="fas fa-coins fa-2x" style="color: #0067d4;"></i>
+                            </div>
+                            <div id="totalPendiente" class="h5 totalp mb-0 font-weight-bold text-gray-800 center-align">$0</div>
+                        </div>            
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
