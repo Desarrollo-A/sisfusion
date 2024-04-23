38ced3396591bae888e15171a3b301960d82ede7
OmarMolinaC98 <programador.analista35@ciudadmaderas.com>
2024-04-23 16:03:53 -0600

    Reporte de lote completo

---
+++ application/views/reportes/reporteInfoCompleta_view.php
@@ -0,0 +1,39 @@
+<div class="tab-pane" id="tabLotesCompletos">
+    <div class="col-md-12">
+        <div class="card card-plain">
+            <div class="card-content">
+                <h3 class="card-title center-align">Consulta de historial de lotes por cliente</h3>
+                <p class="card-title pl-1 center-align">En la búsqueda de la <b>columna cliente</b> se debe de considerar acentos y espacios</p>
+                <div class="material-datatables">
+                    <div class="material-datatables">
+                        <div class="form-group">
+                            <table id="tablaHistorialCompleta" class="table">
+                                <thead>
+                                    <tr>
+                                        <th>ID CLIENTE</th>
+                                        <th>ID LOTE</th>
+                                        <th>PROYECTO</th>
+                                        <th>DESCRIPCION</th>
+                                        <th>NOMBRE CONDOMINIO</th>
+                                        <th>NOMBRE LOTE</th>
+                                        <th>CLIENTE</th>
+                                        <th>FECHA DE APARTADO</th>
+                                        <th>COPROPIETARIO(S)</th>
+                                        <th>REFERENCIA</th>
+                                        <th>ASESOR</th>
+                                        <th>COORDINADOR</th>
+                                        <th>GERENTE</th>
+                                        <th>ESTATUS</th>
+                                        <th>ESTATUS DE CONTRATACION ACTUAL</th>
+                                        <th>SEDE</th>
+                                        <th>TIPO PROCESO</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
