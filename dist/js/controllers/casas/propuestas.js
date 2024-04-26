fa200a5b99ec2578a69d128069833f082506f4a2
rimtzg-maderas <154539918+rimtzg-maderas@users.noreply.github.com>
2024-04-26 13:49:20 -0600

    Bugfixes

--- dist/js/controllers/casas/propuestas.js
+++ dist/js/controllers/casas/propuestas.js
@@ -43,7 +43,7 @@ function show_form(propuesta) {
     form.fields = [
         new HiddenField({ id: 'idPropuesta', value: propuesta.idPropuesta }),
         new HiddenField({ id: 'idProcesoCasas', value: idProcesoCasas }),
-        new SelectField({ id: 'notaria', label: 'Notaria',  placeholder: 'Selecciona una opcion', data: notarias, value: propuesta.notaria, width:'6' }),
+        new SelectField({ id: 'notaria', label: 'Notaria',  placeholder: 'Selecciona una opcion', data: notarias, value: propuesta.id_notaria, width:'6' }),
         new DateField({ id: 'fecha', label: 'Fecha de firma',  placeholder: 'Elige una fecha', value: propuesta.fechaFirma, width:'6' }),
         new NumberField({ id: 'costo', label: 'Costo',  placeholder: 'Ingresa el dato', value: propuesta.costo })
     ]
