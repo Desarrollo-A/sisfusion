55f32b0ce4a8d1bce73870f2fbc2bb22b895e62a
rimtzg-maderas <154539918+rimtzg-maderas@users.noreply.github.com>
2024-05-22 17:36:22 -0600

    Format number currency

--- dist/js/controllers/casas/ingresar_adeudos.js
+++ dist/js/controllers/casas/ingresar_adeudos.js
@@ -40,13 +40,9 @@ function set_adeudo(data) {
     form.show()
 }
 
-const formatter = new Intl.NumberFormat('en-US', {
+const formatter = new Intl.NumberFormat('es-MX', {
   style: 'currency',
-  currency: 'USD',
-
-  // These options are needed to round to whole numbers if that's what you want.
-  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+  currency: 'MXN',
 });
 
 let columns = [
