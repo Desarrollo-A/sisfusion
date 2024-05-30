c7005274ba8fa7a4f094dc54dc816d7fab08df1f
rimtzg-maderas <154539918+rimtzg-maderas@users.noreply.github.com>
2024-05-30 15:52:27 -0600

    Bugfixes

--- dist/js/controllers/pagos_casas/lista_valida_documentos.js
+++ dist/js/controllers/pagos_casas/lista_valida_documentos.js
@@ -17,7 +17,9 @@ let columns = [
     { data: 'documento' },
     { data: 'archivo' },
     { data: function(data){
-        return data.fechaModificacion.substring(0, 16)
+        if(data.fechaModificacion){
+            return data.fechaModificacion.substring(0, 16)
+        }
     } },
     { data: function(data){
         let view_button = ''
