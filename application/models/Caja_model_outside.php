a385c3b171e6aca4a8294c3c080459b27d264a30
cesarprogramador41 <programador.analista41@ciudadmaderas.com>
2024-06-05 16:42:37 -0600

    Se agregan los campos de codigo postal, y rfc cuando se selecciono

--- application/models/Caja_model_outside.php
+++ application/models/Caja_model_outside.php
@@ -1598,7 +1598,9 @@
                 cl.interior,
                 cl.cp,
                 cl.calle,
-                cl.localidad
+                cl.localidad, 
+                cl.regimen_fac,
+                cp_fac
 
             FROM
                 clientes cl
