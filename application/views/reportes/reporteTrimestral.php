8ff06e76977c4a33e3dd0de2c037355abd91778e
AudryAC <programador.analista12@ciudadmaderas.com>
2024-07-04 08:58:55 -0600

    reporte trimestral

--- application/views/reportes/reporteTrimestral.php
+++ application/views/reportes/reporteTrimestral.php
@@ -1,10 +1,15 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
 <link href="<?= base_url() ?>dist/css/datatableNFilters.css" rel="stylesheet"/>
+<style>
+    .titleCustom:hover{
+        box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+    }
+</style>
 <body>
     <div class="wrapper">
         <?php $this->load->view('template/sidebar'); ?>
 
-        <div class="modal fade" id="detailPayments" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+        <div class="modal fade" id="detailPayments" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-md">
                 <div class="modal-content" style="background-color:#ecedf0">
                     <div class="modal-header">
