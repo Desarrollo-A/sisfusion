3affb658d732ebc5769ad6283161152a99bdd95d
cesarprogramador41 <programador.analista41@ciudadmaderas.com>
2024-07-09 14:08:42 -0600

    .

--- application/views/reestructura/reestructura_view.php
+++ application/views/reestructura/reestructura_view.php
@@ -156,7 +156,6 @@
 				</div>
 			</div>
 		</div>
-        <!--KEEP HISTORIAL MOVIMIENTOS-->
         <div class="modal fade" id="modal_historial" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
             <div class="modal-dialog modal-md modal-dialog-scrollable" role="document">
                 <div class="modal-content">
