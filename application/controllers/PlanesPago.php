ef99dcd5420aab28f8a25fdb9648f80a722701de
rimtzg-maderas <154539918+rimtzg-maderas@users.noreply.github.com>
2024-08-15 15:44:21 -0600

    Update PlanesPago.php

--- application/controllers/PlanesPago.php
+++ application/controllers/PlanesPago.php
@@ -27,14 +27,34 @@ class PlanesPago extends CI_Controller {
 
             $saldo_insoluto = $plan->monto;
 
-            foreach ($pagos as $num_pago => $pago) {
+            foreach ($pagos as $num_pago =>$pago) {
                 if(!isset($pago->pagado)){
+                    // print_r($saldo_insoluto);
+
                     return [$num_plan, $num_pago, $saldo_insoluto];
                 }
 
-                $saldo_insoluto = $pago->capital;
+                $saldo_insoluto -= $pago->capital;
             }
         }
+    }
+
+    public function getMensualidadMasAlta($planes){
+        $mensualidad = 0;
+
+        foreach ($planes as $num_plan => $plan) {
+            $pagos = json_decode($plan->dumpPlan);
+
+            foreach ($pagos as $num_pago =>$pago) {
+                if(!isset($pago->pagado)){
+                    if($pago->total > $mensualidad){
+                        $mensualidad = $pago->total;
+                    }
+                }
+            }
+        }
+
+        return $mensualidad;
     }
 
     private function aplicar_pago($pago){
@@ -55,47 +75,144 @@ class PlanesPago extends CI_Controller {
             $dias_atraso = $interval->days + 1;
         }
         
-        $moratorios = ( ($saldo * 0.04) / 30 ) * $dias_atraso;
-
+        $moratorios = round( ( ($saldo * 0.04) / 30 ) * $dias_atraso, 2, PHP_ROUND_HALF_DOWN );
+        
         $pago->moratorios = $moratorios;
 
-        if($monto > $pago->moratorios){
-            $monto -= $pago->moratorios;
+        $pago->fechaPago = date('d-m-Y', strtotime($data->fecha));
+        $pago->capitalPactado = round($saldo, 2, PHP_ROUND_HALF_DOWN);
+        $pago->saldoCapital = floatval($pago->capital);
+        $pago->saldoInteres = floatval($pago->interes);
+
+        if($pago->total > $monto){
+            $pago->total = $monto;
         }
 
-        if($monto > $pago->saldoInteres){
-            $monto -= $pago->saldoInteres;
+        $pago->saldoMoratiorios = round($pago->moratorios - $monto, 2, PHP_ROUND_HALF_DOWN);
+        if($pago->saldoMoratiorios < 0){
+            $pago->saldoMoratiorios = 0;
+        }
+        $monto -= $pago->moratorios;
+
+        if($monto > 0){
+            $pago->saldoInteres = $pago->interes - $monto;
+            if($pago->saldoInteres < 0){
+                $pago->saldoInteres = 0;
+            }
+
+            $monto -= $pago->interes;
+
+            if($monto > 0){
+                $pago->saldo = $pago->capitalPactado - $pago->capital;
+
+                $pago->saldoCapital = round($pago->capital - $monto, 2, PHP_ROUND_HALF_DOWN);
+                if($pago->saldoCapital < 0){
+                    $pago->saldoCapital = 0;
+                }
+
+                $monto -= $pago->capital;
+                
+                if($monto < 0){
+                    $monto = 0;
+                }
+            }
         }
 
-        if($monto < $pago->capital){
-            $pago->capital = $monto;
-        }
-
-        $monto -= $pago->total;
+        $pago->capital = round($pago->capital - $pago->saldoCapital, 2, PHP_ROUND_HALF_DOWN);
+        $pago->interes = round($pago->interes - $pago->saldoInteres, 2, PHP_ROUND_HALF_DOWN);
+        $pago->moratorios -= $pago->saldoMoratiorios;
+        $pago->saldo = $pago->capitalPactado - $pago->capital;
 
         return [$pago, $monto];
+    }
+
+    public function insertar_pago_a_capital($pagos, $num_pago, $monto){
+        $viejo = $pagos[$num_pago];
+
+        $fecha = $viejo->fechaPago;
+        $planPago = $viejo->planPago;
+        $pago = $viejo->pago;
+        $capital = $monto;
+        $saldoCapital = 0;
+        $interes = 0;
+        $saldoInteres = 0;
+        $iva = 0;
+        $saldoIva = 0;
+        $moratorios = 0;
+        $saldoMoratiorios = 0;
+        $capitalPactado = $viejo->saldo;
+        $total = $monto;
+        $saldo = $viejo->saldo - $monto;
+
+        $restante = (object) [
+            'fecha' => $fecha,
+            'planPago' => $planPago,
+            'pago' => $pago,
+            'capital' => $capital,
+            'saldoCapital' => $saldoCapital,
+            'interes' => $interes,
+            'saldoInteres' => $saldoInteres,
+            'iva' => $iva,
+            'saldoIva' => $saldoIva,
+            // 'moratorios' => $moratorios,
+            // 'fechaPago' => $fechaPago,
+            // 'saldoMoratiorios' => $saldoMoratiorios,
+            'capitalPactado' => $capitalPactado,
+            'pagado' => 1,
+            'total' => $total,
+            'saldo' => $saldo,
+        ];
+
+
+        array_splice(
+            $pagos,
+            $num_pago + 1,
+            0,
+            [$restante]
+        );
+
+        foreach ($pagos as $key => $pago) {
+            $pago->pago = $key + 1;
+        }
+
+        return $pagos;
     }
 
     private function insertar_pago($pagos, $num_pago, $monto){
         $viejo = $pagos[$num_pago];
 
-        $total = $monto * -1;
-        $capital = $monto * -1;
-        $saldo = $viejo->saldo;
+        $fecha = $viejo->fechaPago;
+        $planPago = $viejo->planPago;
+        $pago = $viejo->pago;
+        $capital = $viejo->saldoCapital;
+        $saldoCapital = 0;
+        $interes = $viejo->saldoInteres;
+        $saldoInteres = 0;
+        $iva = $viejo->saldoIva;
+        $saldoIva = 0;
+        $moratorios = $viejo->saldoMoratiorios;
+        $saldoMoratiorios = 0;
+        $capitalPactado = $viejo->saldo;
+        $total = floatval($viejo->saldoCapital) + floatval($viejo->saldoInteres) + floatval($viejo->saldoIva) + floatval($viejo->saldoMoratiorios);
+        $saldo = $viejo->saldo - $viejo->saldoCapital;
 
         $restante = (object) [
-            'fecha' => $viejo->fecha,
-            'planPago' => $viejo->planPago,
-            'pago' => 0,
+            'fecha' => $fecha,
+            'planPago' => $planPago,
+            'pago' => $pago,
             'capital' => $capital,
+            'saldoCapital' => $saldoCapital,
+            'interes' => $interes,
+            'saldoInteres' => $saldoInteres,
+            'iva' => $iva,
+            'saldoIva' => $saldoIva,
+            // 'moratorios' => $moratorios,
+            // 'fechaPago' => $fechaPago,
+            // 'saldoMoratiorios' => $saldoMoratiorios,
+            'capitalPactado' => $capitalPactado,
+            // 'pagado' => $pagado,
             'total' => $total,
             'saldo' => $saldo,
-            
-            'saldoCapital' => 0,
-            "interes" => "0.00",
-            "saldoInteres" => 0,
-            "iva" => "0.00",
-            "saldoIva" => 0,
         ];
 
 
@@ -143,21 +260,35 @@ class PlanesPago extends CI_Controller {
                 $pago->pagado = 1;
                 $pagos[$num_pago] = $pago;
 
-                if(round($monto, 2) < 0){
+                if($pago->saldoCapital > 0){
                     $pagos = $this->insertar_pago($pagos, $num_pago, $monto);
                 }
 
                 $planes[$num_plan]->dumpPlan = json_encode($pagos);
             }
 
+            if($data->capital){
+                $mensualidad = $this->getMensualidadMasAlta($planes);
+
+                if($monto > $mensualidad){
+                    #print_r("Ingresar a capital: $monto" );
+                    $pagos = $this->insertar_pago_a_capital($pagos, $num_pago, $monto);
+
+                    $planes[$num_plan]->dumpPlan = json_encode($pagos);
+
+                    $monto = 0;
+                }
+            }
+
             #Guardar plan de pagos
-            $this->PlanesPagoModel->savePlanPago($planes[$num_plan]->idPlanPago, $planes[$num_plan]->dumpPlan);
+            # $this->PlanesPagoModel->savePlanPago($planes[$num_plan]->idPlanPago, $planes[$num_plan]->dumpPlan);
 
             if($monto <= 0){
                 print_r($planes[$num_plan]->dumpPlan);
 
                 break;
             }
+
         }
     }
 }
