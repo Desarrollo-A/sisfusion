008d801be9ee4d03592e4763c99819be19f449bf
mgarmarcm <mariadejesus.garduno@ciudadmaderas.com>
2024-08-20 11:41:06 -0600

    Ticket #85898

--- dist/js/controllers/reestructura/reporteCancelaciones.js
+++ dist/js/controllers/reestructura/reporteCancelaciones.js
@@ -32,7 +32,7 @@ function cancelacionTable() {
                 titleAttr: 'Reestructuración',
                 title: 'Reestructuración',
                 exportOptions: {
-                    columns: [0, 1, 2, 3, 4, 5, 6],
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                     format: {
                         header: function (d, columnIdx) {
                             return ' ' + titulos_intxt[columnIdx] + ' ';
