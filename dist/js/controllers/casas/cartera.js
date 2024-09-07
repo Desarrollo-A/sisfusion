082a780f63d9735d500f7070453be06b5d10f18d
AudryAC <programador.analista12@ciudadmaderas.com>
2024-09-07 09:38:41 -0600

    columnas faltantes en Excel

--- dist/js/controllers/casas/cartera.js
+++ dist/js/controllers/casas/cartera.js
@@ -148,7 +148,7 @@ let buttons = [
         titleAttr: 'Descargar archivo excel',
         title:"Originación de cartera",
         exportOptions: {
-            columns: [0, 1, 2, 3, 4],
+            columns: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
             format: {
                 header: function (d, columnIdx) {
                     return $(d).attr('placeholder');
