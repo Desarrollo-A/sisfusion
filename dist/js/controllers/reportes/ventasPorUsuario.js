1b32710c2452e574f29645f261ab7cdfcf55a147
rimtzg-maderas <154539918+rimtzg-maderas@users.noreply.github.com>
2024-10-07 12:44:30 -0600

    Update ventasPorUsuario.js

--- dist/js/controllers/reportes/ventasPorUsuario.js
+++ dist/js/controllers/reportes/ventasPorUsuario.js
@@ -42,7 +42,7 @@ function filltablaLista(beginDate, endDate) {
                     columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
                     format: {
                         header: function (d, columnIdx) {
-                            return ' ' + titulos[columnIdx] + ' ';
+                            return $(d).attr('placeholder').toUpperCase();
                         }
                     }
                 }
