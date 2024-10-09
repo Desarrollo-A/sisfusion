0c3f832260c037281aac9e43a3a4c99ddf8e8c33
AudryAC <programador.analista12@ciudadmaderas.com>
2024-10-08 19:39:38 -0600

    faltante traducir

--- dist/js/controllers/Usuarios/usersByLeader.js
+++ dist/js/controllers/Usuarios/usersByLeader.js
@@ -1,28 +1,9 @@
 $(document).ready( function() {
     construirHead("users_datatable");
     tableUsers();
-    asignarValorColumnasDT();
 })
 
 let titulos = [];
-asignarValorColumnasDT("users_datatable");
-
-// $('#users_datatable thead tr:eq(0) th').each(function (i) {
-//     var title = $(this).text();
-//     if (!excluir_column.includes(title)) {
-//         columnas_datatable.users_datatable.titulos_encabezados.push(title);
-//         columnas_datatable.users_datatable.num_encabezados.push(columnas_datatable.users_datatable.titulos_encabezados.length - 1);
-//     }
-//     let readOnly = excluir_column.includes(title) ? 'readOnly' : '';
-//     if (title !== '') {
-//         $(this).html(`<input type="text" class="textoshead" data-toggle="tooltip" data-placement="top" title="${title}" placeholder="${title}" ${readOnly}/>`);
-//         $('input', this).on('keyup change', function () {
-//             if ($('#users_datatable').DataTable().column(i).search() !== this.value) {
-//                 $('#users_datatable').DataTable().column(i).search(this.value).draw();
-//             }
-//         });
-//     }
-// })
 
 
 function tableUsers(){
@@ -61,11 +42,11 @@ function tableUsers(){
         columns: [
             { data: function (d) {
                 if (d.estatus == 1) {
-                    return '<center><span class="label lbl-green">ACTIVO</span><center>';
+                    return `<center><span class="label lbl-green">${_("activo")}</span><center>`;
                 } else if (d.estatus == 3) {
-                    return '<center><span class="label lbl-orangeYellow">INACTIVO COMISIONADO</span><center>';
+                    return `<center><span class="label lbl-orangeYellow">${_("inactivo")}</span><center>`;
                 } else {
-                    return '<center><span class="label lbl-warning">INACTIVO</span><center>';
+                    return `<center><span class="label lbl-warning">${_("inactivo")}</span><center>`;
                 }
             }
             },
