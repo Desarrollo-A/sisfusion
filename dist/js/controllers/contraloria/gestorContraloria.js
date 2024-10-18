56d3b9f72713af7f20ba16a0dfa6ee8dd3df5466
devosvaldocruzsuarez <programador.analista51@ciudadmaderas.com>
2024-10-18 17:19:46 -0600

    excel

--- dist/js/controllers/contraloria/gestorContraloria.js
+++ dist/js/controllers/contraloria/gestorContraloria.js
@@ -909,7 +909,7 @@ function ConstruirTablaCAmbiarRepresentante(idCondominio) {
                     text: '<i class="fa fa-file-excel-o" aria-hidden="true"></i>',
                     className: 'btn buttons-excel',
                     titleAttr: 'Exportar registros a Excel',
-                    title: "Listado de lotes contratados por intercambio",
+                    title: "Listado de lotes  por representante legal",
                     exportOptions: {
                         columns: [0, 1, 2, 3, 4, 5],
                         format: {
